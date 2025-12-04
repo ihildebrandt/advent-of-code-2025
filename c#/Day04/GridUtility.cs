9185811eb1f6761b86518d223f1e98f19df5d42e
P. Isaac Hildebrandt <p.isaac.hildebrandt@gmail.com>
2025-12-04 12:53:36 -0600

    add day 4

---
+++ c#/Day04/GridUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Day04
+{
+    internal static class GridUtility
+    {
+        public static bool CanAccess(char[][] grid, int x, int y)
+        {
+            var hit = 0;
+            var accessible = true;
+
+            for (var xd = -1; xd <= 1; xd++)
+            {
+                if (x == 0 && xd == -1) continue;
+                if (x == grid[0].Length - 1 && xd == 1) continue;
+
+                for (var yd = -1; yd <= 1; yd++)
+                {
+                    if (y == 0 && yd == -1) continue;
+                    if (y == grid.Length - 1 && yd == 1) continue;
+
+                    if (xd == 0 && yd == 0) continue;
+
+                    if (grid[y + yd][x + xd] == '@') hit++;
+
+                    if (hit >= 4) accessible = false;
+                }
+            }
+
+            return accessible;
+        }
+    }
+}
---
+++ c#/Day04/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using Common;
+using Day04;
+
+var lines = File.ReadAllLines(Input.GetInputPath(4, false))
+    .Select(line => line.ToCharArray())
+    .ToArray();
+var width = lines[0].Length;
+
+var access = new List<(int x,int y)>();
+var total = 0;
+
+do
+{
+    foreach (var coord in access)
+    {
+        lines[coord.y][coord.x] = 'x';
+    }
+    access.Clear();
+
+    for (var y = 0; y < lines.Length; y++)
+    {
+        for (var x = 0; x < width; x++)
+        {
+            if (lines[y][x] != '@') continue;
+
+            if (GridUtility.CanAccess(lines, x, y))
+            {
+                Console.WriteLine($"Accessible: ({x},{y})");
+                access.Add((x, y));
+            }
+        }
+    }
+
+    total += access.Count;
+} while (access.Any());
+
+Console.WriteLine(total);
